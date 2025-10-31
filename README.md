f10971f6a979fbb2bac4f2cb522200dbf6ac2420
David Jozwik <david@djoz.us>
2025-10-31 14:31:10 -0500

    Iteration 1 complete

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# pizza-problem
+# Pizza Problem
+
+This technical assessment problem involves determining how much pizza oven capacity is being used in any given 10-minute window over a time-series.
+
+The most basic first version of this problem does not worry about:
+
+- Available capacity of the pizza ovens. Just how much capacity is being used total. (if 2 pizzas cook simultaneously for 10 minutes, that counts as 20 minutes of usage, 4 for 40 minutes, and so on)
+- Calculating when orders will be ready.
+- Calculating the timeline, this is pre-calculated and provided as input data. Though it may be better to dynamically populate this from items as they are added as opposed to hard-coded.
+
+## Files:
+
+- `original.ts` contains the original problem to be solved
+- `/iterations` folder contains various iterations of the solution as I worked through the problem and refined my approach.
+
+## Usage
+
+You'll need Node.js installed to run the code. You'll also need to run `npm install` to get the dependencies.
+
+In the package.json, you can run each of the solution iterations to see their output. For example:
+
+```bash
+npm run iter1
+```
---
+++ iterations/iteration-1.ts
@@ -0,0 +1,124 @@
+/*
+*   Implement the addItem method so that the final console log meets the following criteria
+*
+*   We have a timeline of time-slots, each slot 10 minutes apart
+*   Our goal is to make it so when an item is added, we calculate how that item effects the timeline.
+*   If an item comes in, to be made (startTime) at 10 and a prepTime of 25, we should:
+*       Apply 10 minutes worth of prep time to slot timeslot with startTime 10,
+        then add 10 to timeslot 20, and finally, 5 to timeslot 30.
+*
+*   Example input/output:
+*
+*   addItem is called with a startTime of 10 and prepTime of 35
+*   output sample:
+
+        { startTime: 0, endTime: 9, usedTime: 0 },
+        { startTime: 10, endTime: 19, usedTime: 10 },
+        { startTime: 20, endTime: 29, usedTime: 10 },
+        { startTime: 30, endTime: 39, usedTime: 10 },
+        { startTime: 40, endTime: 49, usedTime: 5 },
+        { startTime: 50, endTime: 59, usedTime: 0 },
+
+*
+*   addItem is called again with a startTime of 20 and prepTime of 31
+*   output sample:
+
+        { startTime: 0, endTime: 9, usedTime: 0 },
+        { startTime: 10, endTime: 19, usedTime: 10 },
+        { startTime: 20, endTime: 29, usedTime: 20 },
+        { startTime: 30, endTime: 39, usedTime: 20 },
+        { startTime: 40, endTime: 49, usedTime: 15 },
+        { startTime: 50, endTime: 59, usedTime: 1 },
+
+*
+*/
+
+type TimeSlot = {
+	startTime: number;
+	endTime: number;
+	usedTime: number;
+};
+
+type Item = {
+	startTime: number;
+	prepTime: number; // the total prep time for this item
+};
+
+const interval = 10; // the number of minutes between each time slot
+
+const timeSlotTimeline: TimeSlot[] = [
+	{
+		startTime: 0,
+		endTime: 9,
+		usedTime: 0,
+	},
+	{
+		startTime: 10,
+		endTime: 19,
+		usedTime: 0,
+	},
+	{
+		startTime: 20,
+		endTime: 29,
+		usedTime: 0,
+	},
+	{
+		startTime: 30,
+		endTime: 39,
+		usedTime: 0,
+	},
+	{
+		startTime: 40,
+		endTime: 49,
+		usedTime: 0,
+	},
+	{
+		startTime: 50,
+		endTime: 59,
+		usedTime: 0,
+	},
+	{
+		startTime: 60,
+		endTime: 69,
+		usedTime: 0,
+	},
+];
+
+const items: Item[] = [];
+
+const addItem = (item: Item) => {
+	const { startTime, prepTime } = item;
+
+	// Find the time slot where this order's usage should begin to be added at
+	let timeSlotIndex = timeSlotTimeline.findIndex((slot) => {
+		return startTime >= slot.startTime && startTime <= slot.endTime;
+	});
+
+	// It's possible that an incoming item may not be found in the precomputed timeline
+	if (!timeSlotIndex) {
+		throw new Error("No time slot found");
+	}
+
+	let remainingCookTime = prepTime;
+
+	// Step through time slots, adding usage time to each
+	while (remainingCookTime > 0) {
+		// We may only use the max available time for each time slot interval, but we shouldn't overcook the pizza and exceed the remaining cook time!
+		const usageThisSlot = Math.min(remainingCookTime, interval);
+
+		timeSlotTimeline[timeSlotIndex].usedTime += usageThisSlot;
+
+		remainingCookTime -= usageThisSlot;
+
+		timeSlotIndex++;
+	}
+
+	items.push(item);
+};
+
+addItem({ startTime: 10, prepTime: 25 });
+addItem({ startTime: 20, prepTime: 31 });
+
+console.log(timeSlotTimeline);
+console.log(`Total orders today: ${items.length}`);
+console.log(`Total cook time today: ${items.reduce((total, { prepTime }) => total + prepTime, 0)}`);
